59ebef551dca965e670fe721528effa5fe260583
“fab2s35“ <sosorto72@gmail.com>
2024-07-08 12:05:18 -0600

    actuvudad

---
+++ app/src/main/java/g/m/bottomsheet/modal_bottom_sheet_content.kt
@@ -0,0 +1,20 @@
+package g.m.bottomsheet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class modal_bottom_sheet_content : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_modal_bottom_sheet_content)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
